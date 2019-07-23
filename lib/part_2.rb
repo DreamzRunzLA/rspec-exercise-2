7f6cfd450c3faf756a5c0c62bb2e5351498adef4
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-07-22 21:15:30 -0600

    On problem 2.2

--- lib/part_2.rb
+++ lib/part_2.rb
@@ -1,23 +1,27 @@
-def palindrome?(str)
-    return 'fuckguy'
-    # testString = ''
+def palindrome?(string)
+    testString = ''
 
-    # i = string.length - 1
-    # while i >= 0 do
-    #     testString += string[i]
-    #     i -= 1
-    # end
+    i = string.length - 1
+    while i >= 0 do
+        testString += string[i]
+        i -= 1
+    end
 
-    # if testString == string
-    #     return true
-    # else
-    #     return false
-    # end
+    if testString == string
+        return true
+    else
+        return false
+    end
 end
 
 
 def substrings(string)
+    i = string.length - 1
+    ans = []
 
+    while i > 0 do
+        
+    end
 end
 
 
