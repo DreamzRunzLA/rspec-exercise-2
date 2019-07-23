9e6e7a81a0d50608afa0f1dcdead379310f66908
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-07-22 21:11:30 -0600

    Started part 2

---
+++ lib/part_2.rb
@@ -0,0 +1,26 @@
+def palindrome?(str)
+    return 'fuckguy'
+    # testString = ''
+
+    # i = string.length - 1
+    # while i >= 0 do
+    #     testString += string[i]
+    #     i -= 1
+    # end
+
+    # if testString == string
+    #     return true
+    # else
+    #     return false
+    # end
+end
+
+
+def substrings(string)
+
+end
+
+
+def palindrome_substrings(string)
+
+end
