72a32964d9a03ce058bc54d022f659c9954038eb
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-07-22 20:59:29 -0600

    Finished part 1

--- lib/part_1.rb
+++ lib/part_1.rb
@@ -17,17 +17,54 @@ end
 
 
 def merge(hash1, hash2)
-    
+    ans = Hash.new 
+
+    hash1.keys.each do |key|
+        ans[key] = hash1[key]
+    end
+
+    hash2.keys.each do |key|
+        ans[key] = hash2[key]
+    end
+
+    return ans
 end
 
 
 def censor(sentence, curseWords)
+    vowels = 'aeiou'
+    words = sentence.split(' ')
+    ans = []
 
+    words.each do |word|
+        if curseWords.include?(word.downcase)
+            newWord = ''
+            word.each_char do |char|
+                if vowels.include?(char.downcase)
+                    newWord.concat('*')
+                else
+                    newWord.concat(char)
+                end
+            end
+            ans << newWord
+        else
+            ans << word
+        end
+    end
+    return ans.join(' ')
 end
 
 
 def power_of_two?(num)
-
+    powersOfTwo = [1]
+    (1..20).each do |num|
+        powersOfTwo << 2**num
+    end
+    if powersOfTwo.include?(num)
+        return true
+    else
+        return false
+    end
 end
 
 
